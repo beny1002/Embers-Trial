d25e76c84b445c53bcd3a61253c9364f424e914b
Nathan <Phommarathnathan@gmail.com>
2025-01-05 20:52:32 -0800

    imports
    importing and testing background and other classes.

--- core/src/main/java/com/EmbersTrial/Main.java
+++ core/src/main/java/com/EmbersTrial/Main.java
@@ -1,22 +1,50 @@
 package com.EmbersTrial;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main implements ApplicationListener {
+    Texture backgroundTexture;
+    FitViewport viewport;
+
+
     @Override
     public void create() {
-        // Prepare your application here.
+        backgroundTexture = new Texture("background.png");
     }
+    viewport = new FitViewport(8,5);
 
     @Override
     public void resize(int width, int height) {
-        // Resize your application here. The parameters represent the new window size.
+        viewport.update(width, height, true);
+
     }
 
     @Override
     public void render() {
-        // Draw your application here.
+        input();
+        logic();
+        draw();
+    }
+
+    private void input() {
+
+    }
+    private void logic() {
+
+    }
+    private void draw() {
+
     }
 
     @Override
