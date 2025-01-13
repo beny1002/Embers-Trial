f19b00f81489daf113e08f2668e86ca97a0d21b1
Nathan <Phommarathnathan@gmail.com>
2025-01-13 15:36:26 -0800

    cant walk on lava

--- assets/tilemapped.tsx
+++ assets/tilemapped.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.1" name="tilemapped" tilewidth="32" tileheight="32" tilecount="144" columns="12">
- <image source="tile32x32.png" width="400" height="400"/>
+ <image source="../../tile32x32.png" width="400" height="400"/>
  <tile id="4">
   <properties>
    <property name="blocked" value=""/>
@@ -133,6 +133,7 @@
  </tile>
  <tile id="80">
   <properties>
+   <property name="blocked" value=""/>
    <property name="death" value=""/>
   </properties>
   <animation>
@@ -144,6 +145,9 @@
   </animation>
  </tile>
  <tile id="81">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
   <animation>
    <frame tileid="14" duration="100"/>
    <frame tileid="26" duration="200"/>
@@ -152,6 +156,9 @@
   </animation>
  </tile>
  <tile id="92">
+  <properties>
+   <property name="blocked" value=""/>
+  </properties>
   <animation>
    <frame tileid="14" duration="600"/>
    <frame tileid="26" duration="600"/>
